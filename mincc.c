cfb83615bf46b79693f748db1af5c5b1b5f4cf78
yutarofire <yutaro.ono.418@gmail.com>
2021-03-01 23:16:47 +0900

    Accept one number to return

--- mincc.c
+++ mincc.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("expected one argment\n");
+    exit(1);
+  }
+
   printf("  .intel_syntax noprefix\n");
   printf("  .globl main\n");
   printf("main:\n");
-  printf("  mov rax, 42\n");
+  printf("  mov rax, %s\n", argv[1]);
   printf("  ret\n");
 }
--- test.sh
+++ test.sh
@@ -2,9 +2,14 @@ make
 
 echo '=== test start ==='
 
-./mincc > tmp.s
+./mincc 42 > tmp.s
 gcc -o tmp tmp.s
 ./tmp
 echo "42?: $?"
+
+./mincc 123 > tmp.s
+gcc -o tmp tmp.s
+./tmp
+echo "123?: $?"
 
 echo '=== test end ==='
